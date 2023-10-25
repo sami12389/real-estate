3a66c0053c770902dfee75578898f92739fb1bd3
sami12389 <samseifu974@gmail.com>
2023-10-25 13:22:10 -0400

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import {Home, Profile, SignIn, SignOut, About} from "./pages/index"
+import Header from "./components/Header"
 
 const App = () => {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path = "/" element = {<Home/>}>HOME</Route>
         <Route path = "/about" element = {<About/>}>ABOUT</Route>
