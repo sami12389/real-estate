1c1b07f3727e15eb2664f680d3660aa3961a947e
sami12389 <samseifu974@gmail.com>
2023-10-25 22:24:36 -0400

    models and routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 import express from 'express'
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from 'body-parser'
 
-
+dotenv.config()
 const app = express()
 
+// Mongoose Setup
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    app.listen(PORT, ()=>console.log(`Server running on PORT:${PORT}`))
+    // User.insertMany(users)
+    // Post.insertMany(posts)
+}).catch((error)=>console.log(`${error} did not connect`))
